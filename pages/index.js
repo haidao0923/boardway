4b03b9cddae69055d9db24e2b067e56a0f078cf7
Hai Dao <haixdao0923@gmail.com>
2024-03-04 16:48:33 -0500

    add logistic for placing new tiles

--- pages/index.js
+++ pages/index.js
@@ -12,58 +12,6 @@ import image_article_27 from '../public/images/games/article_27.png';
 import image_catan from '../public/images/games/catan.png';
 
 export default function Home() {
-
-
-  class Tile {
-    index;
-    name;
-    value;
-
-    constructor(index, name, value=0) {
-      this.index = index;
-      this.name = name;
-      this.value = value;
-    }
-
-    getStyle() {
-      return {top: tilePosition[this.index].top + 'vh',
-              left: tilePosition[this.index].left + 'vw'}
-    }
-
-    activateTile() {
-      console.log(`Activated tile ${this.index}`);
-      setAlertActive(true);
-      setMoney(money => money += this.value)
-      return 1;
-    }
-
-  }
-
-  let tiles = [
-    new Tile(0, "Go ---->"),
-    new Tile(1, "Empty"),
-    new Tile(2, "C", 3),
-    new Tile(3, "Empty"),
-    new Tile(4, "Empty"),
-    new Tile(5, "Empty"),
-    new Tile(6, "Empty"),
-    new Tile(7, "Empty"),
-    new Tile(8, "Empty"),
-    new Tile(9, "Empty"),
-    new Tile(10, "Empty"),
-    new Tile(11, "Empty"),
-    new Tile(12, "Empty"),
-    new Tile(13, "Empty"),
-    new Tile(14, "Empty"),
-    new Tile(15, "Empty"),
-    new Tile(16, "Empty"),
-    new Tile(17, "Empty"),
-    new Tile(18, "Empty"),
-    new Tile(19, "Empty"),
-    new Tile(20, "Empty"),
-    new Tile(21, "Empty"),
-  ];
-
   class Game {
     constructor(name, releaseYear, maxPlayer, gameDuration, complexity, rating, image, category="", mechanism="") {
       this.name = name;
@@ -75,15 +23,66 @@ export default function Home() {
       this.image = image;
       //this.category = category;
       //this.mechanism = mechanism;
+  
+      this.value = gameDuration;
     }
   }
-
+  
   let games = [
     new Game("7 Wonders", 2010, 7, 30, 2.32, 7.7, image_7_wonders),
     new Game("Above and Below", 2015, 4, 90, 2.53, 7.7, image_above_and_below),
     new Game("Article 27", 2012, 6, 30, 2.11, 6.4, image_article_27),
     new Game("Catan", 1995, 4, 90, 2.29, 7.1, image_catan),
   ];
+  
+  class Tile {
+  
+    constructor(index, game) {
+      this.index = index;
+      this.game = game;
+    }
+  
+    getStyle() {
+      return {top: tilePosition[this.index].top + 'vh',
+              left: tilePosition[this.index].left + 'vw',
+              backgroundColor: this.index == 0 ? "green" : this.game == null ? "black" : "rgb(105, 8, 8)"}
+    }
+  
+    activateTile() {
+      console.log(`Activated tile ${this.index}`);
+      //setAlertActive(true);
+      if (this.game != null) {
+        setMoney(money => money += this.game.value)
+      }
+      return 1;
+    }
+  
+  }
+  
+  const [tiles, setTiles] = useState([
+    new Tile(0, "Go ---->"),
+    new Tile(1, null),
+    new Tile(2, games[0]),
+    new Tile(3, null),
+    new Tile(4, null),
+    new Tile(5, games[1]),
+    new Tile(6, null),
+    new Tile(7, null),
+    new Tile(8, null),
+    new Tile(9, null),
+    new Tile(10, null),
+    new Tile(11, null),
+    new Tile(12, null),
+    new Tile(13, null),
+    new Tile(14, null),
+    new Tile(15, null),
+    new Tile(16, null),
+    new Tile(17, null),
+    new Tile(18, null),
+    new Tile(19, null),
+    new Tile(20, null),
+    new Tile(21, null),
+  ]);
 
   const [currentTile, setCurrentTile] = useState(0);
   const [isRolling, setIsRolling] = useState(false);
@@ -94,6 +93,7 @@ export default function Home() {
   const [money, setMoney] = useState(200);
   const [alertText, setAlertText] = useState("Pick a new tile");
   const [isNewTile, setIsNewTile] = useState(false);
+  const [newGame, setNewGame] = useState(null);
 
 
   // Function to handle the character movement
@@ -115,7 +115,7 @@ export default function Home() {
     const timeToDestination = increment * 1000
     setTimeout(() => {tiles[destinationTile].activateTile(); setIsMoving(false);}, timeToDestination);
     if (destinationTile < currentTile) {
-      //setAlertActive(true);
+      setAlertActive(true);
       setTimeout(() => setMoney(money => money + 200), timeToDestination);
     }
   }
@@ -124,6 +124,18 @@ export default function Home() {
     return (
       <p className={styles.pick_new_game_title}>Pick a new tile</p>
     )
+  }
+
+  function getNewGame() {
+    return games[Math.floor(Math.random() * games.length)]
+  }
+  
+  function placeTile(index) {
+    setIsNewTile(false);
+    let newTiles = [...tiles];
+    newTiles[index].game = newGame;
+    setTiles(newTiles);
+    setNewGame(null);
   }
 
   return (
@@ -145,10 +157,10 @@ export default function Home() {
                   key={index}
                   className={styles.tile}
                   style={tile.getStyle()}
-                  onClick={() => console.log("Clicked on tile: " + tile.index)}
+                  onClick={() => {console.log("Clicked on tile: " + tile.index); placeTile(tile.index); console.log(tiles); console.log(newGame)}}
                 >
-                  <p className={styles.tile_text}>{tile.name}</p>
-                  {tile.value != 0 ? <p className={styles.tile_text}>{`Value: \$${tile.value}`}</p> : <></>}
+                  {index == 0 ? <p className={styles.tile_text}>{"Go --------->"}<br></br><br></br>Pass Go Earn $200</p> : tile.game != null ? <p className={styles.tile_text}>{tile.game.name}</p> : <p className={styles.tile_text}>Empty</p>}
+                  {tile.game != null ? <p className={styles.tile_text}>{`Value: \$${tile.game.value}`}</p> : <></>}
                 </div>
               ))}
           </div>
@@ -168,14 +180,14 @@ export default function Home() {
                 alt="dice"
               />
             </div>
-            <button className={styles.roll_button} disabled={isMoving || alertActive} onClick={() => {roll(); console.log("Clicked")}}>Roll</button>
+            <button className={styles.roll_button} disabled={isMoving || alertActive || isNewTile} onClick={() => {roll(); console.log("Clicked")}}>Roll</button>
           </div>
           {isNewTile ?
           <div className={styles.new_tile_group}>
             <p className={styles.new_tile_text}>Click on a tile to place this game</p>
             <div className={styles.new_tile_image}>
               <Image
-                src={`/images/games/7_wonders.png`} // Route of the image file
+                src={newGame.image} // Route of the image file
                 fill
                 alt="game_promo"
               />
@@ -183,7 +195,7 @@ export default function Home() {
           </div> :
           <div className={styles.new_tile_group}>
             <p className={styles.new_tile_text}>Cost $50</p>
-            <button disabled={money < 50} className={styles.new_tile_button} onClick={() => {setMoney(money => money - 50); setIsNewTile(true);}}>Buy new tile</button>
+            <button disabled={money < 50} className={styles.new_tile_button} onClick={() => {setMoney(money => money - 50); setIsNewTile(true); setNewGame(getNewGame())}}>Buy new tile</button>
           </div>}
           <p className={styles.title_text}>{`BoardWay`}</p>
           <p className={styles.roll_count}>{`Roll #${rollCount + 1}`}</p>
